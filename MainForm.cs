d4d7b524ceee4067366f2e324e24c9efe7b5e107
cepthomas <cepthomas@gmail.com>
2022-04-24 10:20:27 -0500

    Fixed stuck play button. Bar shows exact midi bars.

--- MainForm.cs
+++ MainForm.cs
@@ -558,7 +558,6 @@ namespace MidiStyleExplorer
                     else
                     {
                         StopReq();
-                        //Stop();
                         Rewind();
                     }
                 }
